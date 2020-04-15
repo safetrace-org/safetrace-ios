6878c078a7e84e4be7121b9cd8669198a8adf761
Max Mamis <max@prolificinteractive.com>
2020-04-14 20:35:42 -0400

    Initial Commit

---
+++ ctzntrace-sdk/ctzntrace_sdk.h
@@ -0,0 +1,19 @@
+//
+//  ctzntrace_sdk.h
+//  ctzntrace-sdk
+//
+//  Created by Max Mamis on 4/14/20.
+//  Copyright © 2020 ctzn.org. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ctzntrace_sdk.
+FOUNDATION_EXPORT double ctzntrace_sdkVersionNumber;
+
+//! Project version string for ctzntrace_sdk.
+FOUNDATION_EXPORT const unsigned char ctzntrace_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ctzntrace_sdk/PublicHeader.h>
+
+
