860e8aaea23bebeb33a88ae9c9e8ffc80e799468
Max Mamis <max@prolificinteractive.com>
2020-06-19 18:11:55 -0400

    Integrate with push notification, health check, and user settings endpoints

---
+++ safetrace-sdk/Utility/UIApplication+Version.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+extension UIApplication {
+
+    /// Returns a string that describes the app's short version and build number version
+    ///
+    /// Example: "0.916.0-2343"
+    ///
+    /// Where the semantic version and build number are separated by a -
+    static var clientApplicationVersionDescription: String {
+
+        let semanticVersion = Bundle
+            .main
+            .infoDictionary?["CFBundleShortVersionString"] as? String ?? "error"
+
+        let buildNumberVersion = Bundle
+            .main
+            .infoDictionary?["CFBundleVersion"] as? String ?? "error"
+
+        return "\(semanticVersion)-\(buildNumberVersion)"
+    }
+    
+    static var operatingSystemVersionDescription: String {
+        let osVersion = ProcessInfo().operatingSystemVersion
+        return "\(osVersion.majorVersion).\(osVersion.minorVersion)"
+            + ".\(osVersion.patchVersion)"
+    }
+}
